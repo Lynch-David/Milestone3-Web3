dd2e6f32af95e196f3a35cd5caa371e54810f459
David Lynch <davidlynch1919@gmail.com>
2024-11-14 19:56:19 -0500

    Completed chapter 3

--- HPlusSport.Security.Web/Controllers/HomeController.cs
+++ HPlusSport.Security.Web/Controllers/HomeController.cs
@@ -3,7 +3,14 @@ using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
 namespace HPlusSport.Security.Web.Controllers;
-
+/*Course: 		Web Programming 3
+* Assessment: 	Milestone 4
+* Created by: 	David Lynch - 2275556
+* Date: 		14/11/2024
+* Class Name: 	HomeController.cs
+* Description: 	The home controller that handles overall logic to do with the homescreen.
+* Time Task B):	3
+ */
 public class HomeController : Controller
 {
     public IActionResult Index()
